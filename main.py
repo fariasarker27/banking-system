24551bc4fa8a310516a0f7ef6dd689db45366753
fariasarker27 <122114723+fariasarker27@users.noreply.github.com>
2023-04-30 20:38:41 +0000

    addition of account features

--- main.py
+++ main.py
@@ -1,53 +1,63 @@
 import tkinter as tk
+import tkinter.messagebox as messagebox
+import mysql.connector
 
 class OnlineBankingApp:
     def __init__(self, master):
         self.master = master
         self.master.title("Online Banking System")
+        
+        # Connect to the MySQL database
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="yourusername",
+            password="yourpassword",
+            database="banking"
+        )
+        self.cursor = self.db.cursor()
 
-        # Create a label for the balance
+        # Create a label and entry for the user's name
+        self.name_label = tk.Label(self.master, text="Name:")
+        self.name_label.grid(row=0, column=0, padx=10, pady=10)
+        self.name_entry = tk.Entry(self.master)
+        self.name_entry.grid(row=0, column=1, padx=10, pady=10)
+
+        # Create a label and entry for the user's PIN
+        self.pin_label = tk.Label(self.master, text="PIN:")
+        self.pin_label.grid(row=1, column=0, padx=10, pady=10)
+        self.pin_entry = tk.Entry(self.master, show="*")
+        self.pin_entry.grid(row=1, column=1, padx=10, pady=10)
+
+        # Create a button to create a new account
+        self.create_button = tk.Button(self.master, text="Create Account", command=self.create_account)
+        self.create_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+        # Create a button to login
+        self.login_button = tk.Button(self.master, text="Login", command=self.login)
+        self.login_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        # Create a label for the user's balance
         self.balance_label = tk.Label(self.master, text="Balance: $0.00")
-        self.balance_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        self.balance_label.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
         # Create a label and entry for the deposit amount
         self.deposit_label = tk.Label(self.master, text="Deposit amount:")
-        self.deposit_label.grid(row=1, column=0, padx=10, pady=10)
+        self.deposit_label.grid(row=5, column=0, padx=10, pady=10)
         self.deposit_entry = tk.Entry(self.master)
-        self.deposit_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.deposit_entry.grid(row=5, column=1, padx=10, pady=10)
 
         # Create a button to deposit funds
-        self.deposit_button = tk.Button(self.master, text="Deposit", command=self.deposit_funds)
-        self.deposit_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+        self.deposit_button = tk.Button(self.master, text="Deposit", command=self.deposit_funds, state="disabled")
+        self.deposit_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
         # Create a label and entry for the withdrawal amount
         self.withdrawal_label = tk.Label(self.master, text="Withdrawal amount:")
-        self.withdrawal_label.grid(row=3, column=0, padx=10, pady=10)
+        self.withdrawal_label.grid(row=7, column=0, padx=10, pady=10)
         self.withdrawal_entry = tk.Entry(self.master)
-        self.withdrawal_entry.grid(row=3, column=1, padx=10, pady=10)
+        self.withdrawal_entry.grid(row=7, column=1, padx=10, pady=10)
 
         # Create a button to withdraw funds
-        self.withdrawal_button = tk.Button(self.master, text="Withdraw", command=self.withdraw_funds)
-        self.withdrawal_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+        self.withdrawal_button = tk.Button(self.master, text="Withdraw", command=self.withdraw_funds, state="disabled")
+        self.withdrawal_button.grid(row=8, column=0, columnspan=2, padx=10, pady=10)
 
-    def deposit_funds(self):
-        amount = float(self.deposit_entry.get())
-        self.balance += amount
-        self.balance_label.config(text=f"Balance: ${self.balance:.2f}")
-        self.deposit_entry.delete(0, tk.END)
-
-    def withdraw_funds(self):
-        amount = float(self.withdrawal_entry.get())
-        if amount <= self.balance:
-            self.balance -= amount
-            self.balance_label.config(text=f"Balance: ${self.balance:.2f}")
-        else:
-            tk.messagebox.showerror("Error", "Insufficient funds")
-        self.withdrawal_entry.delete(0, tk.END)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = OnlineBankingApp(root)
-    root.mainloop()
-
-
-
+        # Create a logout button
